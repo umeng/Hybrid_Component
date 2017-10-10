9edb4f32ad02ffa740ed9fe768c55e48910e2eaf
wf90163@alibaba-inc.com <wf90163@alibaba-inc.com>
2017-10-10 09:52:10 +0800

    fix

--- ios/WebView/Hybrid_Component/Hybrid_Component/UMHybridBridge/UMPushModule.m
+++ ios/WebView/Hybrid_Component/Hybrid_Component/UMHybridBridge/UMPushModule.m
@@ -15,7 +15,9 @@ static UMPushModule *umengHyhrid = nil;
 
 + (BOOL)execute:(NSString *)parameters webView:(UIWebView *)webView {
     NSString *prefix = @"umpush";
+     NSLog(@"11111");
     if ([parameters hasPrefix:prefix]) {
+        NSLog(@"22222");
         if (nil == umengHyhrid) {
             umengHyhrid = [[UMPushModule alloc] init];
         }
@@ -26,11 +28,11 @@ static UMPushModule *umengHyhrid = nil;
         if ([functionName isEqualToString:@"getDeviceId"]) {
             [umengHyhrid getDeviceId:args webView:webView];
         } else {
-            SEL selector = NSSelectorFromString([NSString stringWithFormat:@"%@:", functionName]);
+              SEL selector = NSSelectorFromString([NSString stringWithFormat:@"%@:webView:", functionName]);
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
             if ([umengHyhrid respondsToSelector:selector]) {
-                [umengHyhrid performSelector:selector withObject:args];
+                [umengHyhrid performSelector:selector withObject:args withObject:webView];
             }
 #pragma clang diagnostic pop
         }
@@ -63,6 +65,188 @@ static UMPushModule *umengHyhrid = nil;
     [webView stringByEvaluatingJavaScriptFromString:callBack];
     
 }
+- (void)addTag:(NSArray *)arguments webView:(UIWebView *)webView{
+      NSLog(@"333");
+     NSString *arg0 = [arguments objectAtIndex:0];
+     NSString *cb = [arguments objectAtIndex:1];
+    [UMessage addTags:arg0 response:^(id  _Nonnull responseObject, NSInteger remain, NSError * _Nonnull error) {
+        int code = -1;
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *respobj = responseObject;
+                // 授权信息
+                if ([[respobj allKeys] containsObject:@"success"]) {
+                    if ([[respobj objectForKey:@"success"] isEqualToString:@"ok"]) {
+                        code = 200 ;
+                    }
+                }
+                if ([[respobj allKeys] containsObject:@"remain"]) {
+                    remainnum = [[respobj objectForKey:@"remain"] intValue];
+                }
+            }
+        }
+        [self handleRemainResult:code remain:remainnum callbackFunction:cb webView:webView];
+//        callback(code, remainnum);
+        
+    }];
+}
+- (void)delTag:(NSArray *)arguments webView:(UIWebView *)webView{
+   
+    NSString *arg0 = [arguments objectAtIndex:0];
+    NSString *cb = [arguments objectAtIndex:1];
+    [UMessage deleteTags:arg0 response:^(id  _Nonnull responseObject, NSInteger remain, NSError * _Nonnull error) {
+        int code = -1;
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *respobj = responseObject;
+                // 授权信息
+                if ([[respobj allKeys] containsObject:@"success"]) {
+                    if ([[respobj objectForKey:@"success"] isEqualToString:@"ok"]) {
+                        code = 200 ;
+                    }
+                }
+                if ([[respobj allKeys] containsObject:@"remain"]) {
+                    remainnum = [[respobj objectForKey:@"remain"] intValue];
+                }
+            }
+        }
+        [self handleRemainResult:code remain:remainnum callbackFunction:cb webView:webView];
+        //        callback(code, remainnum);
+        
+    }];
+}
+- (void)listTag:(NSArray *)arguments webView:(UIWebView *)webView{
+     NSString *cb = [arguments objectAtIndex:0];
+    [UMessage getTags:^(NSSet * _Nonnull responseTags, NSInteger remain, NSError * _Nonnull error) {
+        int code = -1;
+        NSString *tags = @"[";
+       
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseTags isKindOfClass:[NSSet class]]) {
+                NSArray * tagsarray = [responseTags allObjects];
+//                for (int i = 0; i < tagsarray.count; i++) {
+//                    NSString *str = tagsarray[i];
+                 tags = [tagsarray componentsJoinedByString:@","];
+//                }
+                tags = [tags stringByAppendingString:@"]"];
+            }
+        }
+       
+        NSLog(@"%@",tags);
+        [self handleTagsResult:tags callbackFunction:cb webView:webView];
+//        callback(code,tags);
+    }];
+}
+- (void)addAlias:(NSArray *)arguments webView:(UIWebView *)webView{
+    NSString *arg0 = [arguments objectAtIndex:0];
+     NSString *arg1 = [arguments objectAtIndex:1];
+    NSString *cb = [arguments objectAtIndex:2];
+    [UMessage addAlias:arg0 type:arg1 response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
+        int code = -1;
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *respobj = responseObject;
+                // 授权信息
+                if ([[respobj allKeys] containsObject:@"success"]) {
+                    if ([[respobj objectForKey:@"success"] isEqualToString:@"ok"]) {
+                        code = 200 ;
+                    }
+                }
+            }
+        }
+        [self handleAliasResult:code callbackFunction:cb webView:webView];
+      
+    }];
+}
+- (void)delAlias:(NSArray *)arguments webView:(UIWebView *)webView{
+    NSString *arg0 = [arguments objectAtIndex:0];
+    NSString *arg1 = [arguments objectAtIndex:1];
+    NSString *cb = [arguments objectAtIndex:2];
+    [UMessage removeAlias:arg0 type:arg1 response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
+        int code = -1;
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *respobj = responseObject;
+                // 授权信息
+                if ([[respobj allKeys] containsObject:@"success"]) {
+                    if ([[respobj objectForKey:@"success"] isEqualToString:@"ok"]) {
+                        code = 200 ;
+                    }
+                }
+            }
+        }
+        [self handleAliasResult:code callbackFunction:cb webView:webView];
+        
+    }];
+}
+- (void)setAlias:(NSArray *)arguments webView:(UIWebView *)webView{
+    NSString *arg0 = [arguments objectAtIndex:0];
+    NSString *arg1 = [arguments objectAtIndex:1];
+    NSString *cb = [arguments objectAtIndex:2];
+    [UMessage setAlias:arg0 type:arg1 response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
+        int code = -1;
+        int remainnum = 0;
+        if (error) {
+            code = error.code;
+        }else{
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *respobj = responseObject;
+                // 授权信息
+                if ([[respobj allKeys] containsObject:@"success"]) {
+                    if ([[respobj objectForKey:@"success"] isEqualToString:@"ok"]) {
+                        code = 200 ;
+                    }
+                }
+            }
+        }
+        [self handleAliasResult:code callbackFunction:cb webView:webView];
+        
+    }];
+}
+- (void)handleAliasResult:(NSInteger)retCode  callbackFunction:(NSString *)function webView:(UIWebView *)webView
+{
+    if (function.length == 0) {
+        return;
+    }
+  
+    
+    
+    NSString *callBack = [NSString stringWithFormat:@"%@(%ld)", function, retCode];
+    [webView stringByEvaluatingJavaScriptFromString:callBack];
+}
+- (void)handleTagsResult:(NSString *)tags callbackFunction:(NSString *)function webView:(UIWebView *)webView
+{
+    if (function.length == 0) {
+        return;
+    }
+    
+    NSString *callBack = [NSString stringWithFormat:@"%@('%@\')", function, tags];
+    [webView stringByEvaluatingJavaScriptFromString:callBack];
+}
+- (void)handleRemainResult:(NSInteger)retCode remain:(NSInteger)remain callbackFunction:(NSString *)function webView:(UIWebView *)webView
+{
+    if (function.length == 0) {
+        return;
+    }
+  
+    
 
-
+    NSString *callBack = [NSString stringWithFormat:@"%@(%ld,%ld)", function, retCode, remain];
+    [webView stringByEvaluatingJavaScriptFromString:callBack];
+}
 @end
